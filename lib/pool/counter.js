addisonElliott/mongodb-core

WIP: PoC of CMAP

lib/pool/counter.js
@@ -1,0 +1,12 @@
+'use strict';
+
+function* counter(seed) {
+  let count = seed || 0;
+  while (true) {
+    const newCount = count;
+    count += 1;
+    yield newCount;
+  }
+}
+
+module.exports = { counter };

parisholley/node-mongodb-native

 - WIP: more updates
 - WIP more changes
 - updating pool and runner to properly handle new test
 - updating to follow spec compliance with allowing not-ready conns
 - adding spec compliance so connect does not block acquisition
 - adding spec tests
 - remove old pool spec tests
 - updating spec tests
 - update cmap runner for latest tests
 - updaating spec definitions
 - ensure that pool spec tests are not run in pre-node8 env
 - move file up a directory to avoid being run
 - make pool node 4 compliant
 - updating test runner for cmap to use node 4
 - move tests back to where it belongs
 - updating stale connection test
 - ACTUAL updated to pool-checkout-no-stale
 - make Connection be identicial to placeholder connection
 - remove old parts of spec like WaitQueueFullError and waitQueueSize
 - make sure waitQueueTimeoutMS default is 0 in waitQueue

lib/pool/counter.js
@@ -1,0 +1,12 @@
+'use strict';
+
+function* makeCounter(seed) {
+  let count = seed || 0;
+  while (true) {
+    const newCount = count;
+    count += 1;
+    yield newCount;
+  }
+}
+
+module.exports = { makeCounter };